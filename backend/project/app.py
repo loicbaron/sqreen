b48413adde19938a9f3d06be6019287b321faac1
Loïc Baron <loic.baron@gmail.com>
2020-07-15 02:29:39 +0200

    build(run): add run and app

---
+++ backend/project/app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+# Sqreen library monitors the security of this application
+# It is an In-App Web Application Firewall
+# more info: https://sqreen.com/
+import sqreen
+sqreen.start()
+
+import datetime
+import os
+from flask import Flask, jsonify, render_template, request, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+
+from project.config.database import config, db_session, init_db
+# from project.controllers.obj import obj_controller
+from project.controllers.notifications import notifications_controller
+
+def create_app():
+    app = Flask(__name__, static_url_path='', instance_relative_config=True)
+    app.config['SQREEN_NOTIFICATIONS_KEY'] = os.environ.get("SQREEN_NOTIFICATIONS_KEY")
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    with app.app_context():
+        init_db()
+
+    @app.teardown_appcontext
+    def shutdown_session(exception=None):
+        db_session.remove()
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    app.register_blueprint(notifications_controller)
+    # app.register_blueprint(obj_controller)
+    
+    return app
+
---
+++ backend/run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import os
+from dotenv import load_dotenv
+
+from project.app import create_app
+
+"""
+WARNING: This is a development server. Do not use it in a production deployment.
+Use a production WSGI server instead.
+"""
+
+if __name__ == '__main__':
+    # Default Environment variables 
+    # are defined in config/environment.py
+    # load dotenv in the base root
+    APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+    dotenv_path = os.path.join(APP_ROOT, '.env')
+    load_dotenv(dotenv_path)
+    # WARNING: This is a development server. Do not use it in a production deployment.
+    # Use a production WSGI server instead.
+    create_app().run(host='0.0.0.0', port=8000, debug=True)
