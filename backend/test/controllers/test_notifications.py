ddd629b4fa52f5ade343f5de6a4b309ece783b2e
Loïc Baron <loic.baron@gmail.com>
2020-07-15 02:35:01 +0200

    test(notifications): add notifications controller

---
+++ backend/test/controllers/test_notifications.py
@@ -0,0 +1,21 @@
+import json
+import os
+from flask import jsonify
+
+def test_notifications_forbidden(client):
+    rv = client.post('/api/notifications', json={
+        'test': 'will be forbidden'
+    })
+    json_data = rv.get_json()
+    assert rv.status_code == 403
+
+def test_notifications_allowed(client):
+    req_sig = '9d101d2bf630748679226b767d2031634c520390ff0e926afc09bc65a05bfdb2'
+    rv = client.post(
+        '/api/notifications', 
+        headers={'X-Sqreen-Integrity': req_sig},
+        data='4567'
+    )
+    json_data = rv.get_json()
+    assert rv.status_code == 200
+    
