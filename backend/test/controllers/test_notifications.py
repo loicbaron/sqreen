f42dc52d1283b061407cbf66de2b7d3a8c22e6e7
Loic Baron <loic.baron@gmail.com>
2020-07-16 13:53:26 +0200

    test(notifications): refactor notifications test

--- backend/test/controllers/test_notifications.py
+++ backend/test/controllers/test_notifications.py
@@ -2,14 +2,14 @@ import json
 import os
 from flask import jsonify
 
-def test_notifications_forbidden(client):
+def test_notifications_without_signature_forbidden(client):
     rv = client.post('/api/notifications', json={
         'test': 'will be forbidden'
     })
     json_data = rv.get_json()
     assert rv.status_code == 403
 
-def test_notifications_allowed(client):
+def test_notifications_signature_ok(client):
     req_sig = '9d101d2bf630748679226b767d2031634c520390ff0e926afc09bc65a05bfdb2'
     rv = client.post(
         '/api/notifications', 
