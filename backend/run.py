d553ddf104fbd0af95cdefef24b9e0a0926c57e8
Loic Baron <loic.baron@gmail.com>
2020-07-20 01:32:54 +0200

    fix(wsgi): fix APP_ROOT path

--- backend/run.py
+++ backend/run.py
@@ -12,7 +12,7 @@ Use a production WSGI server instead.
 if __name__ == '__main__':
     # Environment variables are defined in .env
     # load dotenv from the base root
-    APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+    APP_ROOT = os.path.join(os.path.dirname(__file__), '.')   # refers to application_top
     dotenv_path = os.path.join(APP_ROOT, '.env')
     load_dotenv(dotenv_path)
     # WARNING: This is a development server. Do not use it in a production deployment.
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -2,10 +2,9 @@
 import os
 from dotenv import load_dotenv
 
-from project.app import create_app
-
-APP_ROOT = os.path.join(os.path.dirname(__file__), '..')   # refers to application_top
+APP_ROOT = os.path.join(os.path.dirname(__file__), '.')   # refers to application_top
 dotenv_path = os.path.join(APP_ROOT, '.env')
 load_dotenv(dotenv_path)
 
+from project.app import create_app
 app = create_app()
